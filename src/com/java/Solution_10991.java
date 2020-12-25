cbf71d478c0e657371ef239ea505d176b9fd9f4b
taehoondev <gnsxorla1@gmail.com>
2020-12-26 08:13:44 +0900

    feat : Solution_10991

---
+++ src/com/java/Solution_10991.java
@@ -0,0 +1,21 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_10991 {
+    public static void main(String[] args) {
+
+        Scanner inputNumber = new Scanner(System.in);
+        int N = inputNumber.nextInt();
+
+        for (int i = 0; i<N; i++) {
+            for (int j = i+1; j<N; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k<=i; k++) {
+                System.out.print('*'+" ");
+            }
+            System.out.println();
+        }
+    }
+}
