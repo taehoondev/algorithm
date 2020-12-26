15df495eab8591207ee55ae526ea71e4fff7a051
taehoondev <gnsxorla1@gmail.com>
2020-12-26 20:47:16 +0900

    feat : Solution_1152

---
+++ src/com/java/Solution_1152.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution_1152 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String message = scan.nextLine();
+
+        StringTokenizer st = new StringTokenizer(message," ");
+        System.out.println(st.countTokens());
+    }
+}
