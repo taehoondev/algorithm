65cba54948ac8174db8d6855fd6ad5867abc5b8e
taehoondev <gnsxorla1@gmail.com>
2020-12-29 15:10:57 +0900

    feat : Solution_2739

---
+++ src/com/java/Solution_2739.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_2739 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i<10; i++) {
+            System.out.println(n+" "+'*'+" "+i+" "+'='+" "+(n*i));
+        }
+    }
+}
