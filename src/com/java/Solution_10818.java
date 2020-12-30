dd3262c261fd839b51563dcb46760af17a656e4a
taehoondev <gnsxorla1@gmail.com>
2020-12-31 06:57:10 +0900

    feat : Solution_10818

---
+++ src/com/java/Solution_10818.java
@@ -0,0 +1,19 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution_10818 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i<n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]+" "+arr[n-1]);
+    }
+}
