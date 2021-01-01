b367fd88be04b2cf3aa223ade653c6054b68a225
taehoondev <gnsxorla1@gmail.com>
2021-01-01 09:36:45 +0900

    feat : Solution_2163

---
+++ src/com/java/Solution_2163.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_2163 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(n*m-1);
+    }
+}
