ba538f6eb137a66c836a629c6a540deebdb6a4d4
taehoondev <gnsxorla1@gmail.com>
2021-01-04 08:39:38 +0900

    feat : Solution_5565

---
+++ src/com/java/Solution_5565.java
@@ -0,0 +1,18 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_5565 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        int[] arr = new int[10];
+        for (int i = 0; i<10; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 1; i<10; i++) {
+            sum+=arr[i];
+        }
+        System.out.println(arr[0]-sum);
+    }
+}
