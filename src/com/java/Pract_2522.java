970f8252c4532b2c02459b23cd74951cf8f632b9
taehoondev <gnsxorla1@gmail.com>
2021-01-16 19:56:47 +0900

    feat : Pract_2522

---
+++ src/com/java/Pract_2522.java
@@ -0,0 +1,31 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Pract_2522 {
+    public static void main(String[] args) {
+
+        Scanner inputNumber = new Scanner(System.in);
+        int N = inputNumber.nextInt();
+
+        for (int i = 0; i<N; i++) {
+            for (int j = i; j+1<N; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k<=i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i<N; i++) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k<N; k++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
