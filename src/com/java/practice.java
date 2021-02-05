b160c6a88162a3f0cb7627c3476cd028adb1fb2a
taehoondev <gnsxorla1@gmail.com>
2021-02-05 16:10:30 +0900

    feat : Solution_11720

--- src/com/java/practice.java
+++ src/com/java/practice.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println(a*(b%10));
-        System.out.println(a*(b%100/10));
-        System.out.println(a*(b/100));
-        System.out.println(a*b);
+        if (a<b) {
+            System.out.println('<');
+        }
+        else if (a>b) {
+            System.out.println('>');
+        }
+        else System.out.println("==");
     }
 }
