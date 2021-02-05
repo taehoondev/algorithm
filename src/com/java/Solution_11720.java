90f3d9c8c1f9ea43eb849c423a3bcf872eaef152
taehoondev <gnsxorla1@gmail.com>
2021-02-05 15:10:02 +0900

    feat : practice

--- src/com/java/Solution_11720.java
+++ src/com/java/Solution_11720.java
@@ -1,4 +1,19 @@
 package com.java;
 
+import java.util.Scanner;
+
 public class Solution_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextInt();
+        long number = sc.nextInt();
+        long sum = 0;
+        while (number > 0) {
+            long a = number % 10;
+            sum += a;
+            number /= 10;
+        }
+        sc.close();
+        System.out.println(sum);
+    }
 }
