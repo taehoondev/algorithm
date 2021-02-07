d36c33537986c2c6f5bf83fe25655872ed0b101a
taehoondev <gnsxorla1@gmail.com>
2021-02-07 17:11:34 +0900

    feat : Solution_10809

---
+++ src/com/java/Solution_10809.java
@@ -0,0 +1,45 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_10809 {
+
+    /*
+
+    1. 스캐너로 문자 입력받기
+    2. indexOf 사
+
+     */
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        String S = sc.nextLine();
+        System.out.print((S.indexOf('a'))+" ");
+        System.out.print((S.indexOf('b'))+" ");
+        System.out.print((S.indexOf('c'))+" ");
+        System.out.print((S.indexOf('d'))+" ");
+        System.out.print((S.indexOf('e'))+" ");
+        System.out.print((S.indexOf('f'))+" ");
+        System.out.print((S.indexOf('g'))+" ");
+        System.out.print((S.indexOf('h'))+" ");
+        System.out.print((S.indexOf('i'))+" ");
+        System.out.print((S.indexOf('j'))+" ");
+        System.out.print((S.indexOf('k'))+" ");
+        System.out.print((S.indexOf('l'))+" ");
+        System.out.print((S.indexOf('m'))+" ");
+        System.out.print((S.indexOf('n'))+" ");
+        System.out.print((S.indexOf('o'))+" ");
+        System.out.print((S.indexOf('p'))+" ");
+        System.out.print((S.indexOf('q'))+" ");
+        System.out.print((S.indexOf('r'))+" ");
+        System.out.print((S.indexOf('s'))+" ");
+        System.out.print((S.indexOf('t'))+" ");
+        System.out.print((S.indexOf('u'))+" ");
+        System.out.print((S.indexOf('v'))+" ");
+        System.out.print((S.indexOf('w'))+" ");
+        System.out.print((S.indexOf('x'))+" ");
+        System.out.print((S.indexOf('y'))+" ");
+        System.out.print((S.indexOf('z'))+" ");
+    }
+}
