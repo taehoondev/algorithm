95bf9a20063266bb2f1fed7cecda245ecad07d5f
taehoondev <gnsxorla1@gmail.com>
2021-02-09 23:32:58 +0900

    feat : practice_2908

---
+++ src/com/java/Practice_2908.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/*
+1. 배열로 받아야함
+
+*/
+public class Practice_2908 {
+    public static void main(String[] args) {
+        List<String> arr = new ArrayList<String>();
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        arr.add(a);
+        String b = sc.next();
+        arr.add(b);
+
+    }
+}
