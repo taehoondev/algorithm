b56b8a38627fb85beb4c1ea02c2f1467151b98b6
taehoondev <gnsxorla1@gmail.com>
2021-02-19 10:43:16 +0900

    feat : Solution_1012

---
+++ src/com/java/Solution_1012.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1012 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float c = sc.nextFloat();
+        System.out.printf("%f",c);
+    }
+}
