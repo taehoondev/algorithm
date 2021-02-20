df4717dc744f8821622d3f8af5e0a1baaf8b6953
taehoondev <gnsxorla1@gmail.com>
2021-02-20 20:56:52 +0900

    feat : Solution_1040

---
+++ src/com/java/Solution_1040.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution_1040 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+
+        System.out.println(-a);
+    }
+}
