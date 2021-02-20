ed2e3a87bcc233607158dc27756f6b4aa4882c5f
taehoondev <gnsxorla1@gmail.com>
2021-02-20 12:57:01 +0900

    feat : Solution_1036

---
+++ src/com/java/Solution_1036.java
@@ -0,0 +1,15 @@
+package com.java;
+
+import javax.management.relation.InvalidRelationTypeException;
+import java.util.Scanner;
+
+public class Solution_1036 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        char a = scanner.nextLine().charAt(0);
+
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
