207d1eee2a9559824708329312e01fd2e45f1a28
taehoondev <gnsxorla1@gmail.com>
2021-02-20 21:26:06 +0900

    feat : Solution_1041

---
+++ src/com/java/Solution_1027.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Solution_1027 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine(); // 10
+
+
+        String c = sc.nextLine();
+    }
+}
---
+++ src/com/java/Solution_1041.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1041 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int b = Integer.valueOf(a.charAt(0));
+        int c = b+1;
+        char d = (char)c;
+        System.out.println(d);
+    }
+}
