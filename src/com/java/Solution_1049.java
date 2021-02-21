0a939e321b8e98ff07d99949b53c58d787c4c6d7
taehoondev <gnsxorla1@gmail.com>
2021-02-21 15:02:41 +0900

    feat : Solution_1049

---
+++ src/com/java/Solution_1049.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1049 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a>b) System.out.println(1);
+        else System.out.println(0);
+    }
+}
