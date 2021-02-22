f8e79390b288143aa3d64c3038cf9ba49d151ea5
taehoondev <gnsxorla1@gmail.com>
2021-02-22 13:04:58 +0900

    feat : Solution_1071

---
+++ src/com/java/Solution_1071.java
@@ -0,0 +1,23 @@
+package com.java;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Solution_1071 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int a = scanner.nextInt();
+            if (a!=0) {
+            System.out.println(a);
+            }
+            else break;
+        }
+    }
+}
