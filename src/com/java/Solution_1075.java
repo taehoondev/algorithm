a9ef7e1590be475b484247dca599ce570748baa5
taehoondev <gnsxorla1@gmail.com>
2021-02-23 09:46:17 +0900

    feat : Solution_1075

---
+++ src/com/java/Solution_1075.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1075 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        for (int i = a-1; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
