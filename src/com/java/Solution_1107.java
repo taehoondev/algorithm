b9e9decd65d12d2e56432cf0334db6b963d71a17
taehoondev <gnsxorla1@gmail.com>
2021-03-04 17:39:12 +0900

    feat : Solution_1107

---
+++ src/com/java/Solution_1107.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import java.io.PrintStream;
+
+public class Solution_1107 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            System.out.print("print");
+        }
+    }
+}
