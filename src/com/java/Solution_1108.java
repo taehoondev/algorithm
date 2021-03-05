6f3e344ebab8752a058c2b7eb060cc8867993acb
taehoondev <gnsxorla1@gmail.com>
2021-03-05 18:07:12 +0900

    feat : Solution_1108

---
+++ src/com/java/Solution_1108.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class Solution_1108 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.print("hello");
+        }
+        System.out.print(" ");
+        for (int j = 0; j < 30; j++) {
+            System.out.print("world");
+        }
+    }
+}
