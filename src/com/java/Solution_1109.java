a14c3672eeff9e9bf2e6ef2b9d17df7a7a19e40c
taehoondev <gnsxorla1@gmail.com>
2021-03-06 19:53:34 +0900

    feat : Solution_1109

---
+++ src/com/java/Solution_1109.java
@@ -0,0 +1,20 @@
+package com.java;
+
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class Solution_1109 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        int b = scanner.nextInt();
+        String x = scanner.nextLine();
+        String c = scanner.nextLine();
+        double d = scanner.nextDouble();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
