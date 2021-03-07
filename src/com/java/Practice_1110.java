c924c42fea4f3af2769f649fa5a0df0a9cd62058
taehoondev <gnsxorla1@gmail.com>
2021-03-07 09:49:32 +0900

    feat : Solution_1110

---
+++ src/com/java/Practice_1110.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Practice_1110 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
