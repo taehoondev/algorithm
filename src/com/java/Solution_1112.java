214507204187abf287618dd549efac69f9193c67
taehoondev <gnsxorla1@gmail.com>
2021-03-09 18:09:55 +0900

    feat : Solution_1112

---
+++ src/com/java/Solution_1112.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import javafx.scene.transform.Scale;
+
+import java.util.Scanner;
+
+public class Solution_1112 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a+" "+b);
+    }
+}
