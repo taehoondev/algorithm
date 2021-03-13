de7f2af7e028a08f09ec89bc02c13488f8059e59
taehoondev <gnsxorla1@gmail.com>
2021-03-13 22:40:27 +0900

    feat : Solution_1116

---
+++ src/com/java/Solution_1116.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1116 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+"+"+b+"="+(a+b));
+        System.out.println(a+"-"+b+"="+(a-b));
+        System.out.println(a+"*"+b+"="+(a*b));
+        System.out.println(a+"/"+b+"="+(a/b));
+    }
+}
