3f5167fd4562f47fb824628138a1c5024b4ae8ce
taehoondev <gnsxorla1@gmail.com>
2021-03-15 21:41:48 +0900

    feat : Solution_1117

---
+++ src/com/java/Solution_1117.java
@@ -0,0 +1,12 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1117 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.printf("%.02f",a*b);
+    }
+}
