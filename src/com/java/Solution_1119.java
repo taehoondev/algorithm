511e287638e10dc4e11c96b17c5292d1af9e8ef0
taehoondev <gnsxorla1@gmail.com>
2021-03-17 23:15:37 +0900

    feat : Solution_1119

---
+++ src/com/java/Solution_1119.java
@@ -0,0 +1,11 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1119 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a*24);
+    }
+}
