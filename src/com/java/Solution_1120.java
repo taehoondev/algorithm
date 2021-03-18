b9299962cf243d66bc893204a9cae4dfa125b110
taehoondev <gnsxorla1@gmail.com>
2021-03-18 21:28:49 +0900

    feat : Solution_1120

---
+++ src/com/java/Solution_1120.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class Solution_1120 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        System.out.printf("%.2f",(a+b+c)/3);
+    }
+}
