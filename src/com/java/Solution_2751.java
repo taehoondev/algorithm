d266039edd44d682bfb9812da99aec8a8af093bf
taehoondev <gnsxorla1@gmail.com>
2021-03-21 20:11:49 +0900

    feat : Solution_2751

--- src/com/java/Solution_2751.java
+++ src/com/java/Solution_2751.java
@@ -4,20 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Solution_2751 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> arr = new ArrayList<Integer>();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            arr.add(Integer.parseInt(br.readLine()));
+            arr.add(Integer.parseInt(bufferedReader.readLine()));
         }
         Collections.sort(arr);
+
         for (int i = 0; i < n; i++) {
-            System.out.println(arr.get(i));
+            stringBuilder.append(arr.get(i)+"\n");
         }
+        System.out.print(stringBuilder.toString());
     }
 }
