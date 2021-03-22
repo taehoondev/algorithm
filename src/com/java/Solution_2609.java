4523e81f95d64cc396916840f86b6ee1fbef9482
taehoondev <gnsxorla1@gmail.com>
2021-03-22 11:43:51 +0900

    feat : Solution_2609

--- src/com/java/Solution_2609.java
+++ src/com/java/Solution_2609.java
@@ -1,31 +1,35 @@
 package com.java;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Solution_2609 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        StringTokenizer st = new StringTokenizer(sc.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int allDouble = 1;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int min;
 
-        for (int i = 1; i < a + 1; i++) {
-            if (a % i == 0 && b % i == 0) {
-                arr.add(i);
+        if (a >= b) {
+            for (int i = b; i >= 1; i--) {
+                if (a % i == 0 && b % i == 0) {
+                    min = i;
+                    System.out.println(i);
+
+                    System.out.println(i*(a/i)*(b/i));
+
+                    break;
+                }
             }
         }
-        int mini = arr.get(arr.size() - 1);
-        System.out.println(mini);
-
-        for (int i = 0; i < arr.size() - 1; i++) {
-            allDouble *= arr.get(i);
+        if (a < b) {
+            for (int i = a; i >= 1; i--) {
+                if (b % i == 0 && a % i == 0) {
+                    min = i;
+                    System.out.println(i);
+                    System.out.println(i*(a/i)*(b/i));
+                    break;
+                }
+            }
         }
-        System.out.println(mini * (a / mini) * (b / mini));
     }
 }
