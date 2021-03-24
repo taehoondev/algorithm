464378a822e8dacf9b3744e77c66a3ec66d49df7
taehoondev <gnsxorla1@gmail.com>
2021-03-24 16:50:02 +0900

    feat : Solution_1526

---
+++ src/com/java/Solution_1526.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Solution_1526 {
+    public static void hello() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+}
