c842ea1d02d2bd6f1e181db49c31d7237db11d33
taehoondev <gnsxorla1@gmail.com>
2021-03-24 18:02:05 +0900

    feat : Solution_1526

--- src/com/java/Solution_1526.java
+++ src/com/java/Solution_1526.java
@@ -1,11 +1,26 @@
 package com.java;
 
-public class Solution_1526 {
-    public static void hello() {
-        System.out.println("hello");
-    }
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution_1526 {
+    public static void output() {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i]=scanner.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[max]<arr[i]) {
+                max=i;
+            }
+        }
+        System.out.println(max+1);
+    }
     public static void main(String[] args) {
-        hello();
+        output();
     }
 }
