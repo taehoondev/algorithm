46a2c157e5c1dd6358137e6962e59ab7f7602858
taehoondev <gnsxorla1@gmail.com>
2021-03-25 21:32:09 +0900

    feat : Solution_1531

---
+++ src/com/java/Solution_1531.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class Solution_1531 {
+    public static int print() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(print());
+    }
+}
